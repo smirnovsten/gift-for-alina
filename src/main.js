23b7e9b54de1c5c26672065c9241466790752223
stensmirnov <stensmirnov1@gmail.com>
2025-12-27 16:49:54 +0200

    promise me i want to make it work

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import AOS from 'aos'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-AOS.init({
-  once: true,
-})
